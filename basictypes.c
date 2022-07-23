492373951a6ee598f39cc69b8a5040c175bd5e8b
fernando <fernando.cr100888@gmail.com>
2022-07-23 12:09:16 -0400

    actualizaciónes and hello world on C++

---
+++ basictypes.c
@@ -0,0 +1,7 @@
+
+/* Basic Types: */
+    (A) Aritmeticos int 2 o 4 Bytes /*permite rangos de -32,768 a 32.768 o -2,147,483,648 a 2,147,483,647*/
+                                    /*los rangos van desde números negativos hasta números positivos, pero existe una manera de usar sólo números positivos*/
+                                    unsigned int /*esto permite usar sólo números positivos. permite que los rangos vayan de 0 a 65,535 o desde 0 a 4,294,967,295*/
+    /*conocer el límite de las variables en C es muy importante, porque si no conocemos nuestros límites */
+    (B) De punto flotante float
--- helloworld.c
+++ helloworld.c
@@ -5,3 +5,4 @@ int main() /*main function*/
     printf("hello world"); /*code inside the main function*/
     return 0; /*Completion of the main function*/
 }
+
